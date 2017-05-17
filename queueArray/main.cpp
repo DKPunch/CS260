18d92c3434ebf1755c58d0a83944e3ffcbefd1f7
Joseph Schell <alejandro1one@hotmail.com>
2017-05-17 00:21:33 -0700

    Add files via upload

---
+++ queueArray/main.cpp
@@ -0,0 +1,48 @@
+#include "queue.h"
+
+Queue::Queue(int size) :
+	//size{size},
+	qArray{new char[size]},		// Auto create new queue array as long as "size"
+	front{-1},					// new array starts the front before 0
+	rear{-1}					// new array starts the rear before 0 also
+{
+}
+
+Queue::~Queue(void)
+{
+	delete[] qArray;
+}
+
+bool Queue::isEmpty()
+{
+	if (front == -1 && rear == -1)	// if both values are -1, queue is empty
+	{
+		return true;
+	}
+	else
+	{
+		return false;	// any other values make the queue not empty
+	}
+}
+
+bool Queue::isFull() 
+{
+	return rear == SIZE - 1;
+}
+
+ostream& operator<<(ostream& out, Queue& queue)
+{
+	out << '[';
+	if (queue.isEmpty())
+		out << "empty";
+	else
+		for (int i = 0; i <= queue.rear; i++)
+			out << queue.qArray[i] << (i < queue.rear ? ", " : "");
+	out << ']' << endl;
+	return out;
+}
+
+int main()
+{
+	return 0;
+}
---
+++ queueArray/queue.h
@@ -0,0 +1,33 @@
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+#include <ostream>
+
+using namespace std;
+
+static const int	SIZE{5};
+
+class Queue
+{
+public:
+	Queue(int size);
+	~Queue();
+	bool isEmpty();
+	bool isFull();
+	bool enqueue(char ch);
+	bool dequeue(char& ch);
+
+
+
+	friend ostream& operator<<(ostream& out, Queue& queue);
+
+private:
+	//int		size;
+	char	*qArray;
+	int		front;
+	int		rear;
+};
+
+//ostream& operator<<(ostream& out, Queue *queue);
+
+#endif
