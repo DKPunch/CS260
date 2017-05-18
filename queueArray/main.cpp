3029bb85f083307af871f29b916b51bb1d05c7c4
Joseph Schell <alejandro1one@hotmail.com>
2017-05-17 22:55:32 -0700

    Update main.cpp

--- queueArray/main.cpp
+++ queueArray/main.cpp
@@ -1,5 +1,3 @@
-/* Created by: Joseph Schell
-*/
 #include <iostream>
 #include "queue.h"
 
@@ -23,75 +21,87 @@ bool Queue::isEmpty()
 	}
 	else
 	{
-		return false;	// any other values make the queue not empty
+		return false;		// any other values make the queue not empty
 	}
 }
 
 void Queue::enqueue(int num)
 {
-	if ((rear + 1) % SIZE == front)			// Check if queue is full
+	if ((rear + 1) % SIZE == front)				// Check if queue is full
 	{
-	cout << "The queue is full!! \n";
+		cout << "The queue is full!! \n";
 	}
 	else
 	{
-		if (front == -1)					// first element inserted
+		if (front == -1)				// first element inserted
 		{
 			front = 0;
 		}
-		//insert element at rear
-		rear = (rear + 1) % SIZE;
-		qArray[rear] = num;
+		// inserts element at rear
+		rear = (rear + 1) % SIZE;			// allows the rear to reset to the 0 element in the array after array is full
+		qArray[rear] = num;				// this is key to making this a circular array
 	}
 }
 
 void Queue::dequeue()
 {
-	if (isEmpty())
+	if (isEmpty())						// Check if queue is full
+	{
 		cout << "Queue is empty\n";
+	}
 	else
-		//only one element
-		if (front == rear)
-			front = rear = -1;
+	{
+		if (front == rear)				// if only one element in array
+		{
+			front = rear = -1;			// set front and rear to -1, dequeueing rear front element
+		}
 		else
-			front = (front + 1) % SIZE;
+		{
+			front = (front + 1) % SIZE;		// else, move front in circular array motion, 
+		}						// similar to rear from above function
+	}
 }
 
-//function to show the element at front
-void Queue::showfront()
+void Queue::showFront()
 {
 	if (isEmpty())
+	{
 		cout << "Queue is empty\n";
+	}
 	else
-		cout << "element at front is:" << qArray[front];
+	{
+		cout << "element at front is:" << qArray[front];	// shows first element, first to pop on dequeue
+	}
 }
 
 //function to display queue
 void Queue::displayQueue()
 {
 	if (isEmpty())
+	{
 		cout << "Queue is empty\n";
+	}
 	else
 	{
 		int i;
-		if (front <= rear)
+		if (front <= rear)					// first case, front element smaller than rear
 		{
-			for (i = front; i <= rear; i++)
+			for (i = front; i <= rear; i++)			// traverse array
 				cout << qArray[i] << " ";
 		}
 		else
 		{
 			i = front;
-			while (i < SIZE)
+			while (i < SIZE)				// while front is less than SIZE
 			{
-				cout << qArray[i] << " ";
+				cout << qArray[i] << " ";		// traverses one side of circular array, after position of front
 				i++;
 			}
 			i = 0;
-			while (i <= rear)
+			while (i <= rear)				// while element is less than rear...
 			{
-				cout << qArray[i] << " ";
-				i++;
+				cout << qArray[i] << " ";		// increment everything up to rear position
+				i++;					// which is everything behind front that wasnt printed in prev loop
 			}
 		}
 	}
@@ -102,6 +112,7 @@ int main()
 	Queue q;
 	int choice, value, flag = 1;
 
+	// display menu
 	while (flag == 1)
 	{
 		cout << "\n1.enqueue 2.dequeue 3.showfront 4.displayQueue 5.exit\n";
@@ -114,7 +125,7 @@ int main()
 			break;
 		case 2: q.dequeue();
 			break;
-		case 3: q.showfront();
+		case 3: q.showFront();
 			break;
 		case 4: q.displayQueue();
 			break;
@@ -123,6 +134,5 @@ int main()
 		}
 	}
 
-	system("pause");
 	return 0;
 }
